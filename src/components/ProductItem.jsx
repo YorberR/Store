731b7a559460fd799f99fb5aeb08f1da4a45dc74
YorberR <yorberrojas15@gmail.com>
2023-03-13 12:52:17 -0400

    Agregado manejo del estado

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
-import cart from "@icons/bt_add_to_cart.svg" 
+import addToCart from "@icons/bt_add_to_cart.svg" 
 
 const ProductItem = () => {
+
+	const [cart, setCart] = useState([]);
+
+	const haldleClick = () => {
+		setCart([])
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,8 +18,8 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src={cart} alt="" />
+				<figure onClick={haldleClick}>
+					<img src={addToCart} alt="" />
 				</figure>
 			</div>
 		</div>
