710a79ad0cfcc722d6284ac887132ae24f84006e
YorberR <yorberrojas15@gmail.com>
2023-03-23 16:56:56 -0400

    agregando useEffec

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '@styles/Header.scss';
-import Menu from '@componrnts/Menu.jsx';
+import Menu from '@components/Menu.jsx';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
