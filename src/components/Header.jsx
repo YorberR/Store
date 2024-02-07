6a0825cd486a382d4059a254693ff94f0060181d
YorberR <yorberrojas15@gmail.com>
2024-02-07 11:31:48 -0400

    Updated to a newer version of react and removed webpack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from 'react';
 import '../styles/Header.scss';
-import Menu from '@components/Menu.jsx';
-import MyOrder from '@containers/MyOrder';
-import menu from '@icons/icon_menu.svg';
-import logo from '@logos/logo_yard_sale.svg';
-import AppContext from '@context/AppContext';
-import shoppingCart from '@icons/icon_shopping_cart.svg';
+import Menu from './Menu.jsx';
+import MyOrder from '../containers/MyOrder';
+import menu from '../assets/icons/icon_menu.svg';
+import logo from '../assets/logos/logo_yard_sale.svg';
+import AppContext from '../context/AppContext';
+import shoppingCart from '../assets/icons/icon_shopping_cart.svg';
 
 const Header = () => {
 
